ecedfc56ab5094f53ffb16e769b70e8bf04edf66
RafaeloxMC <53903394+RafaeloxMC@users.noreply.github.com>
2023-12-19 17:25:22 +0100

    idek

---
+++ app/src/main/java/dev/peekone/IntroActivity.java
@@ -0,0 +1,14 @@
+package dev.peekone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
